93e9da843332b83b06b805f80767074f24065cb7
printMP <bonez7_2000@yahoo.com>
2025-05-19 19:47:38 -0500

    Ensured other pages were connected to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+import Ecom from './pages/Ecom/Ecom'
+import Feature from './pages/Feature/Feature'
+import Resume from './pages/Resume/Resume'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      <nav style={{ padding: '1em', background: '#eee'}}>
+        <Link to="/Ecom" style={{ marginRight: 'irem' }}>Ecom </Link>
+        <Link to="/Feature" style={{ marginRight: 'lrem' }}>Feature </Link>
+        <Link to="/Resume">Resume</Link>
+      </nav>
+      <Routes>
+        <Route path="/Ecom" element={< Ecom />} />
+        <Route path="/Feature" element={< Feature />} />
+        <Route path="/Resume" element={< Resume/>} />
+      </Routes>
+    </div>
+
   )
 }
-
-export default App
