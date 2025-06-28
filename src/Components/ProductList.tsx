e7b1c61b69468d55a643c41181548b81dbb33e06
printMP <bonez7_2000@yahoo.com>
2025-06-27 20:38:50 -0500

    Tried working with a .TS extension to avoid conversion & limitations and ended up going back to using JSON instead

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react"
-
+import { useEffect, useState } from "react";
+//import type { Product } from '../types/Product';
 
 const ProductList: React.FC = () => {
+    interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  stock: number;
+  rating: number;
+}
+    const [products, setProducts] = useState<Product[]>([])
+
     useEffect(() => {
-        fetch('/products.json')
-        // .then((res)=> res.json())
-        .then((data) => console.log("This is ",data))
-        .catch()
-    })
+        fetch('/store/products.json')  // JSON file on server
+  .then(res => res.json())     // parse it to JS objects
+  .then((data: Product[]) => {
+    setProducts(data);          // now you have typed JS objects
+    console.log("Products is from useState",data);
+  })
+  .catch(err => console.error(err));
+
+    },[])
 
     return (
         <div>
