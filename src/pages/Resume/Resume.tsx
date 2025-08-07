6ba5aee7ef2228a029f0443a68e2e9aa686c230a
printMP <bonez7_2000@yahoo.com>
2025-08-07 11:11:23 -0500

    Edited resume to make it VERY basic and took out alot, took out just a tiny bit more this time and added to hobbies.

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -13,8 +13,8 @@ Email: MaulinPatel5@gmail.com
 Phone: (423)381-9820
 
 Experience:
-- 3yrs - Sr Technical Advisor & SME at Apple Corp contract through Transcom Inc
 - Recently finished Fullstack Javascript bootcamp (React, React Native, TypeScript, Express.js, Node.js, bootstrap, Ajax,Jquery, CSS, HTML, Vite, MongoDB...etc)
+- 3yrs - Sr Technical Advisor & SME at Apple Corp contract through Transcom Inc
 - 4yrs - Software Eng - For parents' company quite some time ago (maybe 15-20yrs) making business apps in C++ and Java
 - 4yrs - Software Eng - Same scenario as above for another business of their's. In C++/Java.
 Education:
@@ -23,8 +23,8 @@ Education:
 - In search of certifications to expand on my knowledge. I'll be honest, some of them are pretty expensive, and I need a job first to pay for some of them.
 Hobbies:
 - Algos - When I was 15, I decrypted MCI/WorldCom's long distance card algorithm with a pen and a pad! Nothing illegal was done, I didn't use the fruits of my labor
-I also like trying out other languages, scripting, or frameworks: Python, Vue.js, Next.js, Java again, SASS, Tailwind CSS, mySQL, PHP, Kotlin, Visual Basic, Go/Golang`;
-
+I also like trying out other languages, scripting, or frameworks: Python, Vue.js, Next.js, Java again, SASS, Tailwind CSS, mySQL, PHP, Kotlin, Visual Basic, Go/Golang
+I've played sports all my life: Basketball, Volleyball, Track, Soccer, Football, Golf, Baseball....You name it.`;
 return (
     <div style={resumeStyle}>
       <h1><b>Resume:</b></h1>
